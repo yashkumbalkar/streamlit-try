cd4f14724999eb822bd45161b0114f04a45f390e
yashkumbalkar <97614510+yashkumbalkar@users.noreply.github.com>
2025-10-29 16:13:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import streamlit as st
 lst = [1,2]
 
 if st.button("Show List"):
-    # साधारण list दिखाने के लिए
+    
     st.write(data)
 
-    # JSON format में दिखाने के लिए
+    
     st.json(data)
 
 
